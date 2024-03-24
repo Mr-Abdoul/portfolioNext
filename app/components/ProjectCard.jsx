c9467223665b70e6ad3fe0a97d59ecad549f7c55
CODELOCCOL <codeloccol@Abdouls-iMac.local>
2024-03-24 01:54:56 +0100

    project

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -3,24 +3,23 @@ import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from 'next/link';
 import Image from 'next/image';
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
-  const img = require(`../../public${imgUrl}`);
+const ProjectCard = ({ imgUrl, name, description, gitUrl, image }) => {
+  // const img = require(`../../public${imgUrl}`);
  
   return (
     <>
       <div>
-        <div className='h-52 md:h-72 rounded-xl relative group bg-no-repeat  bg-contain bg-center  ' style={{ backgroundImage: `url(${img.default.src})` }}>
+        <div className='h-52 md:h-72 rounded-xl relative group bg-no-repeat  bg-contain bg-center ' style={{ backgroundImage: `url(${image.url})` }}>
+        
           <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 '>
-            <Link href={gitUrl} className='h-14 w-14  rounded-full  border-[#ADB7BE] hover:border-white group/link'>
+            {/* <Link href={gitUrl} className='h-14 w-14  rounded-full  border-[#ADB7BE] hover:border-white group/link'>
               <CodeBracketIcon className='h-10 w-10  text-[#ADB7BE] absolute top-1/2 transform -translate-y-1/2 cursor-pointer group-hover/link:text-white' />
-            </Link>
-            <Link href={previewUrl} className='h-14 w-14   rounded-full  border-[#ADB7BE] hover:border-white group/link'>
-              <EyeIcon className='h-10 w-10  text-[#ADB7BE] absolute top-1/2 transform  -translate-y-1/2 cursor-pointer group-hover/link:text-white' />
-            </Link>
+            </Link> */}
+            
           </div>
         </div>
         <div className='text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4'>
-          <h5 className='text-xl font-semibold mb-2'>{title}</h5>
+          <h5 className='text-xl font-semibold mb-2'>{name}</h5>
           <p className='text-[#ADB7BE]'>{description}</p>
         </div>
       </div>
--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,105 +1,129 @@
 "use client";
-import React, { useState } from 'react';
-import ProjectCard from './ProjectCard';
-import ProjectTag from './ProjectTag';
-import TabButton from './TabButton';
+import React, { useEffect, useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import TabButton from "./TabButton";
 
-
-const projectData = [
-    {
-        id: 1,
-        title: "Js Portfolio Website",
-        description: "Projet 1 description",
-        image: "/imgProjects/img1.png",
-        alt: "suis img1",
-        tag: ["All", "Web"],
-        gitUrl: "/"
-    },
-    {
-        id: 2,
-        title: "Todo liste",
-        description: "Projet 2 description",
-        image: "/imgProjects/img2.png",
-        tag: ["All", "web", "Mobile"],
-        gitUrl: "/"
-    },
-    {
-        id: 3,
-        title: "Calculator Website",
-        description: "Projet 3 description",
-        image: "/imgProjects/img3.png",
-        tag: ["All", "Web"],
-        gitUrl: "/"
-    },
-    {
-        id: 4,
-        title: "React Login page Website",
-        description: "Projet 4 description",
-        image: "/imgProjects/img4.png",
-        tag: ["All", "Web"],
-        gitUrl: "/"
-    },
-    {
-        id: 5,
-        title: "React  Website",
-        description: "Projet 5 description",
-        image: "/imgProjects/img5.png",
-        tag: ["All", "web", "Mobile"],
-        gitUrl: "/"
-    },
-    {
-        id: 6,
-        title: "js jeux Website",
-        description: "Projet 6 description",
-        image: "/imgProjects/img6.png",
-        tag: ["All", "Web", "Mobile"],
-        gitUrl: "/"
-
-    },
-]
+// const projectData = [
+//   {
+//     id: 1,
+//     title: "Js Portfolio Website",
+//     description: "Projet 1 description",
+//     image: "/imgProjects/img1.png",
+//     alt: "suis img1",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//   },
+//   {
+//     id: 2,
+//     title: "Todo liste",
+//     description: "Projet 2 description",
+//     image: "/imgProjects/img2.png",
+//     tag: ["All", "web", "Mobile"],
+//     gitUrl: "/",
+//   },
+//   {
+//     id: 3,
+//     title: "Calculator Website",
+//     description: "Projet 3 description",
+//     image: "/imgProjects/img3.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//   },
+//   {
+//     id: 4,
+//     title: "React Login page Website",
+//     description: "Projet 4 description",
+//     image: "/imgProjects/img4.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//   },
+//   {
+//     id: 5,
+//     title: "React  Website",
+//     description: "Projet 5 description",
+//     image: "/imgProjects/img5.png",
+//     tag: ["All", "web", "Mobile"],
+//     gitUrl: "/",
+//   },
+//   {
+//     id: 6,
+//     title: "js jeux Website",
+//     description: "Projet 6 description",
+//     image: "/imgProjects/img6.png",
+//     tag: ["All", "Web", "Mobile"],
+//     gitUrl: "/",
+//   },
+// ];
 
 const ProjectSection = () => {
-    const [tag, setTag] = useState("All");
-    const handelTagChange = (newTag) =>{
-        setTag(newTag);
+  const [tag, setTag] = useState("All");
+
+  const [data, setData] = useState(null);
+
+  const handelTagChange = (newTag) => {
+    setTag(newTag);
+  };
+//   const filteredProjects = data.filter((project) =>
+//     project.tag.includes(tag)
+//   );
+
+  const fetchData = async () => {
+    let headersList = {
+      Accept: "*/*",
     };
-    const filteredProjects = projectData.filter((project) =>
-    project.tag.includes(tag)
-    )
+
+    let response = await fetch("http://localhost:4000/api/send/getTache", {
+      method: "GET",
+      headers: headersList,
+    });
+
+    let data = await response.json();
+    setData(data)
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(data);
 
   return (
     <>
-    <h1 id='project' className='text-4xl font-blod text-white mb-4'>My Projects </h1> 
-    <div  className='text-white flex flex-row justify-center items-center grap-2 py-6'>
-        <ProjectTag 
-            onClick={handelTagChange} 
-            name="All" 
-            isSelected={tag === "All"}
+      <h1 id="project" className="text-4xl font-blod text-white mb-4">
+        My Projects{" "}
+      </h1>
+      <div className="text-white flex flex-row justify-center items-center grap-2 py-6">
+        <ProjectTag
+          onClick={handelTagChange}
+          name="All"
+          isSelected={tag === "All"}
         />
-        <ProjectTag 
-            onClick={handelTagChange} 
-            name="Web" 
-            isSelected={tag === "Web"}
+        <ProjectTag
+          onClick={handelTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
         />
-        <ProjectTag 
-            onClick={handelTagChange} 
-            name="Mobile" 
-            isSelected={tag === "Mobile"}
+        <ProjectTag
+          onClick={handelTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
         />
-    </div>
-    <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-        {filteredProjects.map((project) => (
-            <ProjectCard key={project.id} 
-            title={project.title} 
-            description={project.description} 
-            imgUrl={project.image}
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {data && data.map((project) => (
+          <ProjectCard
+            key={project.id}
+            name={project.name ? project.name : ''}
+            description={project.description}
+            image={project.image ? project.image: ''}
             alt={project.alt}
             gitUrl={project.gitUrl}
-            previewUrl={project.previewUrl}
-        />))}
-    </div>
+          />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
